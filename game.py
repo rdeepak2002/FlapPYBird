ee8d9349859ab42b6c4a807a1be2d5c35eff34a5
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-06-19 20:32:58 +0800

    improved movement and cleaned up code

--- game.py
+++ game.py
@@ -1,71 +1,72 @@
-import sys, pygame                                                      # imports
+import pygame                                                                   # imports
 
-screenWidth  = 288                                                      # screen width variable
-screenHeight = 512                                                      # screen height variable
+screenWidth  = 288                                                              # screen width variable
+screenHeight = 512                                                              # screen height variable
 
-screen = pygame.display.set_mode((screenWidth, screenHeight))         # set screen size
+screen = pygame.display.set_mode((screenWidth, screenHeight))                   # set screen size
 
 world = {
-    "score" : 0,
-    "background-image" : pygame.image.load("resources/background-day.png")
+  "score"            : 0,
+  "background-image" : pygame.image.load("resources/background-day.png")
 }
 
-bird = {                                                                # bird object
-  "x"            :  127.0,
-  "y"            :  240.0,
-  "width"        :   34.0,
-  "height"       :   24.0,
-  "yVelocity"    :    3.0,
-  "yVelocityMin" :    8.0,
-  "yVelocityMax" :   -9.0,
-  "yAccel"       :   -1.0,
-  "jumpSpeed"    :    8.0,
-  "image"        : pygame.image.load("resources/yellowbird-midflap.png")
+bird = {                                                                        # bird object
+  "x"                  : 127.0,
+  "y"                  : 240.0,
+  "width"              :  34.0,
+  "height"             :  24.0,
+  "yVelocity"          :   3.0,
+  "yVelocityMin"       :  10.0,
+  "yVelocityMax"       :  -12.0,
+  "yAccel"             :  -1.0,
+  "jumpSpeed"          :  10.0,
+  "image"              : pygame.image.load("resources/yellowbird-midflap.png")
 }
 
 def init():
-  gameIcon = pygame.image.load("resources/yellowbird-midflap.png")      # game icon
+  gameIcon = pygame.image.load("resources/yellowbird-midflap.png")              # game icon
   pygame.display.set_icon(gameIcon)
   pygame.display.set_caption("Flappy Bird")
 
-  pygame.init()                                                         # initialize pygame
+  pygame.init()                                                                 # initialize pygame
 
-def checkEvents():                                                      # method to check if mouse is clicked or game is closed    
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:                                     # close game
-          sys.exit()
-      elif event.type == pygame.MOUSEBUTTONUP:                          # mouse pressed
-          bird["yVelocity"] = bird["jumpSpeed"]
+def checkEvents():                                                              # method to check if mouse is clicked or game is closed    
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:                                               # close game
+      sys.exit()
+    elif event.type == pygame.MOUSEBUTTONUP:                                    # mouse pressed
+      bird["yVelocity"] = bird["jumpSpeed"]
 
-def updateSprites():                                                    # method to update images on screen
-    screen.blit(world["background-image"], (0, 0))                      # draw background image
-    birdPos = bird["x"], bird["y"]                                      # get position of bird
-    screen.blit(bird["image"], birdPos)                                 # draw bird 
-    pygame.display.flip()                                               # update screen
+def updateSprites():                                                            # method to update images on screen
+  screen.blit(world["background-image"], (0, 0))                                # draw background image
+  screen.blit(bird["image"], (bird["x"], bird["y"]  ))                          # draw bird 
+  pygame.display.flip()                                                         # update screen
 
 def mover():
-    bird["yVelocity"] += bird["yAccel"]
+  bird["yVelocity"] += bird["yAccel"]
 
-    if(bird["yVelocity"] < bird["yVelocityMax"]):
-        bird["yVelocity"] = bird["yVelocityMax"]
+  if(bird["yVelocity"] < bird["yVelocityMax"]):
+    bird["yVelocity"] = bird["yVelocityMax"]
 
-    if(bird["yVelocity"] > bird["yVelocityMin"]):
-        bird["yVelocity"] = bird["yVelocityMin"]
+  if(bird["yVelocity"] > bird["yVelocityMin"]):
+    bird["yVelocity"] = bird["yVelocityMin"]
 
-    bird["y"] -= bird["yVelocity"]          
+  bird["y"] -= bird["yVelocity"]          
 
-    if(bird["y"] > 488):
-        bird["y"] = 488
+  if(bird["y"] > 488):
+    bird["velocity"] = 0
+    bird["y"] = 488
+  if(bird["y"] < 0):
+    bird["velocity"] = 0
+    bird["y"] = 0
 
-
-def gameLoop():                                                         # game loop which contains logic of the game
-    checkEvents()
-    mover()
-    updateSprites()
+def gameLoop():                                                                  # game loop which contains logic of the game
+  checkEvents()
+  mover()
+  updateSprites()
 
 def main():
   init()
-
   while True:
     gameLoop()
 
